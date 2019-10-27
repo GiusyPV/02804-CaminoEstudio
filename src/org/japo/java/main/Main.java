2664daadcea5d908b153b424de2b761af67fb7d8
Giusy <Giusy@192.168.1.129>
2019-10-27 14:09:14 +0100

    :tada: Primer Commit

---
+++ src/org/japo/java/main/Main.java
@@ -0,0 +1,53 @@
+/* 
+ * Copyright 2019 Josefina Pugliese Vazquez - josefinapwork@gmail.com.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.japo.java.main;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Josefina Pugliese Vazquez
+ */
+public class Main {
+    public static final Scanner SCN
+            = new Scanner(System.in, "Windows-1252")
+                    .useLocale(Locale.ENGLISH).useDelimiter("\\s+");
+
+    public static void main(String[] args) {
+        
+        final double CAMINO = 980;
+        
+        double traslado;
+        double jornadas;
+        
+        try {
+            System.out.print("Desplazamiento diario (km) ...: ");
+            traslado = SCN.nextDouble();
+            
+            jornadas = CAMINO / traslado;
+                        
+            System.out.printf("Días de Clases/Camino ........: %d%n", 
+                    (int) jornadas < jornadas ? (int) jornadas + 1 : (int) jornadas);
+            
+        } catch (Exception e) {
+            
+            System.out.println("Error: Entrada incorrecta");
+        }
+        
+        
+    }
+}
